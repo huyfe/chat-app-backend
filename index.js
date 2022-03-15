d3682a1ab8e0e759554df158aa25ffea49edc230
huy fe <huyyntran@gmail.com>
2022-03-16 00:12:50 +0700

    Fix issue buffering timed out after mongoose

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 // Import Routes
 const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+// Connect to database mongodb
+mongoose.connect(process.env.DB_CONNECT, () => console.log('connected to db'));
+
+// Middleware
+app.use(express.json());
+app.use(cors());
 
 // Route Middlewares
 app.use('/api/user', authRoute)
