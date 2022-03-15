19215ecafd2dd9018ee6c4f2c56952146dc49a06
huy fe <huyyntran@gmail.com>
2022-03-16 00:56:48 +0700

    Validate req.body with module npm joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,30 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', async (req, res) => {
-    console.log("API Register is working");
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-
     try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }
+        const validation = await schema.validateAsync(req.body);
 
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        const savedUser = await user.save();
+
+        res.status(200).send({ message: 'Registered successfully', data: savedUser });
+    }
     catch (err) {
-        // res.status(400).send(err);
-        res.send(err);
-        console.log(err);
+        res.status(400).send(err.details[0].message);
     }
 });
 
