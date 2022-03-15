99eb8d142cb9722b99e59546fed6821b923c4b9d
huy fe <huyyntran@gmail.com>
2022-03-16 01:18:05 +0700

    Detect module validation.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,27 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-const Joi = require('joi');
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-})
+const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
+    // Validation
+    const { error } = registerValidation(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Create a user belong User model
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
     try {
-        const validation = await schema.validateAsync(req.body);
-
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        })
-
-        const savedUser = await user.save();
-
-        res.status(200).send({ message: 'Registered successfully', data: savedUser });
+        const savedUser = await user.save(); // Save register user into collection users
+        res.send({ message: 'Registered successfully', data: savedUser }); // Send to client message successfully
     }
     catch (err) {
-        res.status(400).send(err.details[0].message);
+        res.status(400).send(err); // Send to client error status and message
     }
 });
 
-
-module.exports = router;
+module.exports = router; // Export in order that index.js can use
